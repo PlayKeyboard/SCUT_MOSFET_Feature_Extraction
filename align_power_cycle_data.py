a34225bd53c64b3269bb4a1ddcaf201dae654247
Mechrevo <849279036@qq.com>
2026-03-02 08:56:24 +0800

    添加17号器件数据用于代码测试

--- align_power_cycle_data.py
+++ align_power_cycle_data.py
@@ -59,7 +59,7 @@ import numpy as np
 DEFAULT_INPUT_ROOT = r'H:\2026.2.10数据\老化数据' #"data"
 DEFAULT_OUTPUT_ROOT = r'G:\深度学习资料\双脉冲实验平台\2026.3.1整理数据输出\aligned_output'  #"data/aligned_output"
 # 可手动填入多个器件编号，如 ["12", "13", "21"]
-DEFAULT_DEVICE_IDS = ['13']
+DEFAULT_DEVICE_IDS = ['17']
 
 
 PC_FILE_RE = re.compile(r"^(\d{8})_PC_(\d+)_(\d+)_ch(\d+)\.mat$", re.IGNORECASE)
@@ -98,7 +98,7 @@ class AlignConfig:
     max_groups: int = 0
     strict_mode: bool = False
     allow_partial_last_group: bool = True
-    partial_min_cycles: int = 3
+    partial_min_cycles: int = 1
 
     @property
     def expected_cycle_points(self) -> int:
